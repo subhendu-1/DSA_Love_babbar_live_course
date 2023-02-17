d5e424ecfab9fdf8d733e700b6d47f088420c7d4
Subhendu Adak <Subhenduadak54@gmail.com>
2023-02-17 23:54:20 +0530

    vector

--- week_3/class-4.cpp
+++ week_3/class-4.cpp
@@ -1,5 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    vector<int>vac;
+    for(int i = 0; i < vac.size(); i++){
+        vac.push_back(i);
+    }
+    for(int i = 0; i < vac.size(); i++){
+     vac.pop_back();
+    }
     return 0;
 }
