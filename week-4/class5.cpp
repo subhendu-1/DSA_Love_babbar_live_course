873d808b89c9df3937e95e32334ee1da3f459c3a
Subhendu Adak <Subhenduadak54@gmail.com>
2023-02-25 23:58:21 +0530

    Selection sort and Bubble Sort

---
+++ week-4/class5.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selectionSort(vector<int>&vac){
+    for(int i = 0; i < vac.size()-1; i++){
+        int minIndex = i;
+        for(int j = i+1; j < vac.size(); j++){
+            if( vac[j] < vac[minIndex] ){
+                minIndex = j;
+                
+            }
+        }
+        swap(vac[minIndex],vac[i]);
+    }
+}
+
+void bubbleSort(vector<int>&vec){
+    for(int i = 0; i < vec.size()-1; i++){
+        bool swapped = false;
+        for(int j = 0; j < vec.size()-i-1; j++){
+            if(vec[j] > vec[j+1]){
+                swapped = true;
+                swap(vec[j],vec[j+1]);
+            }
+        }
+        if(swapped == false){
+            break;
+        }
+    }
+}
+int main(){
+    vector<int>vec{2,6,3,8,9,15};
+    // selectionSort(vec);
+    bubbleSort(vec);
+    for(int i = 0; i < vec.size(); i++){
+        cout << vec[i] << " ";
+    }
+    return 0;
+}
