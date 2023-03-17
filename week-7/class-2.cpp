9ab0d6708df011f757c2d4c3ee28ea3eab83a1f3
Subhendu Adak <Subhenduadak54@gmail.com>
2023-03-17 23:58:53 +0530

    recursion and some problem

--- week-7/class-2.cpp
+++ week-7/class-2.cpp
@@ -101,6 +101,29 @@ int checkKey(int arr[],int n,int i,int key){
    
 }
 
+
+
+
+vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> answer;
+        int i, j;
+        int count[101] = {0, 0, 0};
+		int len = nums.size();
+        for(i = 0; i < len; i++) {
+            count[nums[i]]++;
+        }
+        for(i = 1; i < 101; i++) {
+            count[i] += count[i - 1];
+        }
+        for(i = 0; i < len; i++) {
+            if(nums[i] == 0)
+                answer.push_back(0);
+            else
+                answer.push_back(count[nums[i] - 1]);
+                
+        }
+        return answer;
+    }
 int main(){
     // int n;
     // cout << "Enter the number " ;
@@ -127,6 +150,9 @@ int main(){
 
     int key = 27;
     cout << checkKey(arr,n,i,key);
+
+
+    
 
     return 0;
 }
