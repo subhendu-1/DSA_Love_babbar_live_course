96de4a312d786b1921c378b8eaf8eb24b3a54cfc
Subhendu Adak <Subhenduadak54@gmail.com>
2023-03-25 23:58:20 +0530

    minimub number of element required to reach the terget

---
+++ week-7/class-4.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> &arr, int target){
+    if(target == 0){
+        return 0;
+    }
+    if(target < 0){
+        return INT_MAX;
+    }
+    int mini = INT_MAX;
+    for(int i = 0; i < arr.size(); i++){
+        int ans = solve(arr,target - arr[i]);
+        if(ans < INT_MAX){
+            mini = min(mini,ans);
+        }
+    }
+    return mini;
+}
+
+int main(){
+    vector<int>arr{1,2,3};
+    int target = 5;
+    int ans = solve(arr,target);
+    cout << "minimum number is " << ans << endl;
+}
