68200355d2f2a95464039e46bcc7b6b8c240f95e
Subhendu Adak <Subhenduadak54@gmail.com>
2023-03-26 23:47:44 +0530

    cut into segment

--- week-7/class-4.cpp
+++ week-7/class-4.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<limits.h>
 using namespace std;
 
 int solve(vector<int> &arr, int target){
@@ -11,11 +12,26 @@ int solve(vector<int> &arr, int target){
     int mini = INT_MAX;
     for(int i = 0; i < arr.size(); i++){
         int ans = solve(arr,target - arr[i]);
-        if(ans < INT_MAX){
-            mini = min(mini,ans);
+        if(ans != INT_MAX){
+            mini = min(mini,ans + 1);
         }
     }
     return mini;
+}
+
+// int solve2(vector<int>& arr,int target,int output){
+//     if(output == target){
+//         return 0;
+//     }
+
+//     int mini = INT_MAX;
+
+
+
+// }
+
+int cut_into_segment(int x,int y,int z,int target){
+    
 }
 
 int main(){
