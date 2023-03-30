0711d1bef4e13b640fbe927d4dec25bff8a2c4de
Subhendu Adak <Subhenduadak54@gmail.com>
2023-03-30 23:53:23 +0530

    quick sort

--- week-8/class-2.cpp
+++ week-8/class-2.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
+void quickSort(int arr[],int s,int e){
+
+}
 
 int main(){
+    int arr[] = {2,5,3,8,9,23};
+    int n = 6;
+    int s = 0;
+    int e = n -1;
+    quickSort(arr,s,e);
+
+    for(auto it:arr){
+        cout << it << " ";
+    }
     
     return 0;
 }
