11a35e9da38a29b4add646d44e9f3b9acd821c5f
Subhendu Adak <Subhenduadak54@gmail.com>
2023-04-18 23:28:31 +0530

    print all unique element in string

--- week-10(linked list)/for_exam.cpp
+++ week-10(linked list)/for_exam.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include<map>
 using namespace std;
 
 int main(){
-    int ma = 0;
+    string str;
+    cin >> str;
+    map<char,int>mp;
+    for(int i = 0; i < str.size(); i++){
+        mp[str[i]]++;
+    }
+    map<char,int>::iterator i;
+    for(i = mp.begin(); i != mp.end(); i++){
+        cout << i->first << " ";
+    }
+
     return 0;
     
 }
