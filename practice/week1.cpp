0320fb6cb67ac6c1317c745abc2db99161446144
Subhendu Adak <Subhenduadak54@gmail.com>
2023-04-20 23:57:07 +0530

    week 1 practice

---
+++ practice/week1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    if(cin >> a){
+        cout << "This write" << endl;
+    }
+    if(cout << a << endl){
+        cout << "This write" << endl;
+    }
+    return 0;
+}
