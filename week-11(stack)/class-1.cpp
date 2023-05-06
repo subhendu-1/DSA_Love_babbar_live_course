b586fa7858ecf875ac566f7c7b709fe1dcfa8e56
Subhendu Adak <Subhenduadak54@gmail.com>
2023-05-06 22:19:30 +0530

    In single array tow stack implementaion

--- week-11(stack)/class-1.cpp
+++ week-11(stack)/class-1.cpp
@@ -92,12 +92,114 @@ class Stack{
     
 };
 
+// fill a array using two stack
+
+class stack2{
+    public:
+        int *arr;
+        int size;
+        int top1;
+        int top2;
+
+    stack2(int size){
+        arr = new int[size];
+        this -> size = size;
+        top1 = -1;
+        top2 = size;
+    }
+
+
+    void push1(int data){
+        if((top2-top1) == 1){
+            //spce is not avalible
+            cout << "Stack is Overflow" << endl;
+        }
+        else{
+            top1++;
+            arr[top1] = data;
+        }
+    }
+
+    void pop1(){
+        if(top1 == -1){
+            cout << "Stack is Underflow" << endl;
+        }
+        else{
+            arr[top1] = 0;
+            top1--;
+        }
+    }
+    void push2(int data){
+        if((top2-top1) == 1){
+            //spce is not avalible
+            cout << "Stack is Overflow" << endl;
+        }
+        else{
+            top2--;
+            arr[top2] = data;
+        }
+    }
+
+    void pop2(){
+        if(top2 == size){
+            cout << "Stack is Underflow" << endl;
+        }
+        else{
+            arr[top2] = 0;
+            top2++;
+        }
+    }
+
+
+   void print(){
+    cout << endl;
+    cout << top1 << endl;
+    cout << top2 << endl;
+
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }cout << endl;
+   }
+
+
+};
+
 
 
 int main(){
-    string str = "36255223";
+    // string str = "36255223";
    
-    string ans = Reverse(str);
-    cout << ans << endl;
+    // string ans = Reverse(str);
+    // cout << ans << endl;
 
+
+    stack2 s(10);
+
+    s.push1(10);
+    s.print();
+    s.push1(20);
+    s.print();
+    s.push1(30);
+    s.print();
+    s.push1(40);
+    s.print();
+    s.push1(50);
+    s.print();
+    s.push1(50);
+    s.print();
+    s.push1(50);
+    s.print();
+
+
+    s.push2(100);
+    s.print();
+    s.push2(90);
+    s.print();
+    s.push2(80);
+    s.print();
+    s.push2(70);
+    s.print();
+
+    s.pop2();
+    s.print();
 }
