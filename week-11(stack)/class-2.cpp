a0dbb8ac54b37c411b64cdce7d84a46d032f842d
Subhendu Adak <Subhenduadak54@gmail.com>
2023-05-09 23:14:19 +0530

    insert top element top of the stack into bottom of the stack

---
+++ week-11(stack)/class-2.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+// top most element insert at bottom of the stack using same stack
+
+ void solve(stack<int>&s,int target){
+    if(s.empty()){
+        s.push(target);
+        return;
+    }
+
+    int topelement = s.top();
+    s.pop();
+
+    //Recurtion call
+    solve(s,target);
+
+    // BT
+
+    s.push(topelement);
+}
+
+
+void insertAtBottom(stack<int>&s){
+    if(s.empty()){
+        cout << "Stack is empty" << endl;
+        return;
+    }
+
+    int tartget = s.top();
+    s.pop();
+    solve(s,tartget);
+}
+
+int main(){
+    stack<int>s;
+    for(int i = 0; i < 5; i++){
+        int n;
+        cin >> n;
+        s.push(n);
+    }
+
+    insertAtBottom(s);
+
+    cout << "print stack" << endl;
+
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+
+}
