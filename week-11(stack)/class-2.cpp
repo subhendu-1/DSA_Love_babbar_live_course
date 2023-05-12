9fe35b8e9e4116e1f35be6deffb48cea48558a6c
Subhendu Adak <Subhenduadak54@gmail.com>
2023-05-12 23:29:41 +0530

    stack reverse and sort using recursion

--- week-11(stack)/class-2.cpp
+++ week-11(stack)/class-2.cpp
@@ -22,14 +22,14 @@ using namespace std;
 }
 
 
-void insertAtBottom(stack<int>&s,int target){
+void insertAtBottom(stack<int>&s){
     if(s.empty()){
         cout << "Stack is empty" << endl;
         return;
     }
 
-    // int tartget = s.top();
-    // s.pop();
+    int target = s.top();
+    s.pop();
     solve(s,target);
 }
 
@@ -46,28 +46,76 @@ void reverseStack(stack<int>&s){
 
     reverseStack(s);
 
-    insertAtBottom(s,target);
+    solve(s,target);
+}
+
+// chechk peranthesis is valid or not QUESTION NO IN LEETCODE :- 20
+
+bool checkValid(string s){
+    stack<char>st;
+    for(int i = 0; i < s.length(); i++){
+        //opening brackets
+        char ch = s[i];
+
+        if(ch == '(' || ch == '{' || ch == '['){
+            st.push(ch);
+        }
+
+        else{
+        // closing brackets
+            if(!st.empty()){
+                char chTop = st.top();
+                if((ch == ')' && chTop == '(') || (ch == '}' && chTop == '{') || (ch == ']' && chTop == '[')){
+                    st.pop();
+                }
+
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+
+    }
+
+    if(st.empty()){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 int main(){
-    stack<int>s;
-    for(int i = 0; i < 5; i++){
-        int n;
-        cin >> n;
-        s.push(n);
-    }
+    // stack<int>s;
+    // for(int i = 0; i < 5; i++){
+    //     int n;
+    //     cin >> n;
+    //     s.push(n);
+    // }
 
     // insertAtBottom(s);
 
 
-    reverseStack(s);
+    // reverseStack(s);
 
-    cout << "print stack" << endl;
+    // cout << "print stack" << endl;
 
-    while(!s.empty()){
-        cout << s.top() << " ";
-        s.pop();
+    // while(!s.empty()){
+    //     cout << s.top() << " ";
+    //     s.pop();
+    // }
+
+    string str = "({)";
+    if(checkValid(str)){
+        cout << "valid" << endl;
     }
+    else{
+        cout << "Not valid" << endl;
+    }
+
     cout << endl;
 
 }
